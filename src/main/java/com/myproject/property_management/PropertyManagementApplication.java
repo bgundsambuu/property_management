60b0c29115fb016095cc8639a9aa9d7e74c205b6
Gundee <bgundsambuu@gmail.com>
2018-11-09 22:59:11 -0800

    first push

---
+++ src/main/java/com/myproject/property_management/PropertyManagementApplication.java
@@ -0,0 +1,12 @@
+package com.myproject.property_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyManagementApplication.class, args);
+	}
+}
